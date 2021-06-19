79246745cf26689e69a7f6d126fdcc34a925bf5f
Joey Felipe <joey.felip11@gmail.com>
2021-06-18 20:50:20 -0500

    commit

--- src/modules/cars/ropositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/ropositories/implementations/CategoriesRepository.ts
@@ -7,8 +7,7 @@ import {
 } from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
-  private repository: Repository<Category>
-  
+  private repository: Repository<Category>;
   constructor() {
     this.repository = getRepository(Category);
   }
--- src/modules/cars/ropositories/in-memory/CategoriesRepositoryinMemory.ts
+++ src/modules/cars/ropositories/in-memory/CategoriesRepositoryinMemory.ts
@@ -1,25 +1,28 @@
 import { Category } from "../../entities/Category";
-import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
 
-class CategoriesRepositoryInMemory implements ICategoriesRepository{
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
   categories: Category[] = [];
   async findByName(name: string): Promise<Category> {
     const category = this.categories.find((category) => category.name === name);
-    return category
-    
+    return category;
   }
   async list(): Promise<Category[]> {
-    const categories = this.categories;
-    return categories
+    const { categories } = this;
+    return categories;
   }
   async create({ name, description }: ICreateCategoryDTO): Promise<void> {
     const category = new Category();
 
     Object.assign(category, {
-      name, description
+      name,
+      description,
     });
 
-    this.categories.push(category)
+    this.categories.push(category);
   }
 }
-export { CategoriesRepositoryInMemory }
+export { CategoriesRepositoryInMemory };
